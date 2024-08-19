3b7892c4c85f19ebe888a449f1195814131b98ff
JupiterDespi <yeetgaming06@gmail.com>
2024-08-19 19:51:20 +0800

    Create MovieQuoteInfo.java

---
+++ Chapter 1/MovieQuoteInfo.java
@@ -0,0 +1,11 @@
+public class MovieQuoteInfo{
+
+public static void main(String[] args){
+
+  System.out.println("Quote: Real Eyes Realist Real Lies ");
+  System.out.println("Movie: The boys");
+  System.out.println("Actor: Chase Crawford");
+  System.out.println("Year: 2019");
+
+}
+}
