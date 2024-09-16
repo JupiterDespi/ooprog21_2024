1e4d87fa41e80d29027b389a042a76101c82f0a2
JupiterDespi <yeetgaming06@gmail.com>
2024-09-16 21:55:35 +0800

    FirstAddressExcercise.java

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,13 @@
+public class FirstAddressExercise {
+
+    public static void main(String[] args){
+    displayAddress();
+    }
+
+    public static void displayAddress() {
+    System.out.println("Samsung Electronics");
+    System.out.println("Yeongtong-gu, Suwon, South Korea");
+    System.out.println("1, Block A, Sector 81, Noida Sector 81-201305.");
+    System.out.println("Company: Samsung Electronics");
+    }
+}
